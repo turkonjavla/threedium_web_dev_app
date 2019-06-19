b1371413e1e570da14a2d0760f4ee50845b22f26
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-06-19 15:56:30 +0200

    Add routes for dealing with posts
    - Add get all posts
    - Add get post by id
    - Add remove post
    - Add create post
    - Add comments to posts
    - Add remove comment from post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,228 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
 const router = express.Router();
 
-// @route GET api/posts
-// @desc Test posts
-// @access Public
-router.get('/', async (req, res) => {
-  res.send('Posts route');
+const authMiddleware = require('../../middleware/authMiddleware');
+
+/* Models */
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+
+// @route POST api/posts
+// @desc Create a post
+// @access Private
+router.post('/', [
+  check('title', 'Title is required')
+    .not()
+    .isEmpty(),
+  check('text', 'Text is required')
+    .not()
+    .isEmpty(),
+  authMiddleware
+], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    const newPost = new Post({
+      title: req.body.title,
+      text: req.body.text,
+      headerImgUrl: req.body.headerImgUrl,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    });
+
+    const post = await newPost.save();
+
+    res
+      .status(200)
+      .json(post);
+  }
+  catch (error) {
+    console.error(error.message);
+    res
+      .status(500)
+      .send('Server error');
+  }
 });
+
+// @route GET api/posts
+// @desc Get all posts
+// @access Private
+router.get('/', authMiddleware, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res
+      .status(200)
+      .json(posts);
+  }
+  catch (error) {
+    console.error(error.message);
+    res
+      .status(500)
+      .send('Server error');
+  }
+});
+
+// @route GET api/posts/:id
+// @desc Get post by id
+// @access Private
+router.get('/:id', authMiddleware, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ msg: 'Article not found' });
+    }
+
+    res
+      .status(200)
+      .json(post);
+  }
+  catch (error) {
+    console.error(error.message);
+
+    if (error.kind === 'ObjectId') {
+      return res
+        .status(404)
+        .json({ msg: 'Article not found' });
+    }
+
+    res
+      .status(500)
+      .send('Server error');
+  }
+});
+
+// @route GET api/posts/:id
+// @desc Delete a post by id
+// @access Private
+router.delete('/:id', authMiddleware, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ msg: 'Article not found' });
+    }
+
+    // check if user is owner of post
+    if (post.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: 'You are not authorized to do that' });
+    }
+
+    await post.remove();
+    res
+      .status(200)
+      .json({ msg: "Article removed" });
+  }
+  catch (error) {
+    console.error(error.message);
+
+    if (error.kind === 'ObjectId') {
+      return res
+        .status(404)
+        .json({ msg: 'Article not found' });
+    }
+
+    res
+      .status(500)
+      .send('Server error');
+  }
+});
+
+// @route POST api/posts/comment/:id
+// @desc Comment on an article
+// @access Private
+router.post('/comment/:id', [
+  check('text', 'Comment can not be empty')
+    .not()
+    .isEmpty(),
+  authMiddleware
+], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    const post = await Post.findById(req.params.id);
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    };
+
+    post.comments.unshift(newComment);
+    await post.save();
+
+    res
+      .status(200)
+      .json(post.comments);
+  }
+  catch (error) {
+    console.error(error.message);
+    res
+      .status(500)
+      .send('Server error');
+  }
+});
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Remove comment
+// @access Private
+router.delete('/comment/:id/:comment_id', authMiddleware, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // pull comment from post
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+    // check if comments exists
+    if (!comment) {
+      return res
+        .status(404)
+        .json({ msg: 'Comment does not exist' });
+    }
+
+    // check if user posted the comment
+    if (comment.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: 'You are not authorized to do that' });
+    }
+
+    comment.remove();
+    await post.save();
+
+    res.status(200).json(post.comments);
+  }
+  catch (error) {
+    console.error(error.message);
+    res
+      .status(500)
+      .send('Server error');
+  }
+});
+
 
 module.exports = router;

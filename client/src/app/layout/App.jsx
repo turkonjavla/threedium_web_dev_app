cf1c22b534365bbce641bb5d7b92132ed39afee1
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-06-19 17:50:23 +0200

    Add CssBaseline

--- client/src/app/layout/App.jsx
+++ client/src/app/layout/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { CssBaseline } from '@material-ui/core';
 
 const App = () => {
   return (
     <div className="App">
+      <CssBaseline />
       <h3>Hello world</h3>
     </div>
   );

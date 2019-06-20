2640cab0a6e72013b042d6129294dffcf97a0394
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-06-20 15:39:22 +0200

    Add auth reducer and toastr reducer to root reducer

--- client/src/app/reducers/rootReducer.js
+++ client/src/app/reducers/rootReducer.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { reducer as toastrReducer } from 'react-redux-toastr';
+
+/* Reducers */
+import authReducer from '../../features/auth/authReducer';
 
 const rootReducer = combineReducers({
-  form: formReducer
+  form: formReducer,
+  auth: authReducer,
+  toastr: toastrReducer
 });
 
 export default rootReducer;

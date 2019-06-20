9bfd832aade6ce0eb31749c0453caf3c0923cef4
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-06-20 15:41:41 +0200

    Add useEffect hook to call loadUser every time pages loads or updates

--- client/src/app/layout/App.jsx
+++ client/src/app/layout/App.jsx
@@ -1,16 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 /* MUI Components */
 import { CssBaseline } from '@material-ui/core';
 
 /* Components */
 import NavBar from '../../features/nav/NavBar/NavBar';
+import RegisterForm from '../../features/auth/Register/RegisterForm';
+import { loadUser } from '../../features/auth/authActions';
+import setAuthToken from '../common/util/setAuthToken';
 
-const App = () => {
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
+const App = ({ store }) => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
     <Fragment>
       <CssBaseline />
       <NavBar />
+      <Switch>
+        <Route exact path="/register" component={RegisterForm} />
+      </Switch>
     </Fragment>
   );
 }

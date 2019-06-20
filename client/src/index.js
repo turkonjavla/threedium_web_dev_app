e2db06d74f30bc3db5ee3fdfa784f10636da38d7
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-06-20 11:19:25 +0200

    Initialize redux store and add hot module replacement

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { configureStore } from './app/store/configureStore';
+const store = configureStore();
+
+const rootElement = document.getElementById('root');
+
+let render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+    rootElement
+  )
+};
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render)
+  })
+}
+
+render();
 serviceWorker.unregister();

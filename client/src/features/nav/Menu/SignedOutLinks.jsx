364b45cb1e6e495994e09b40ac4f02c719ab917e
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-06-20 19:04:29 +0200

    Add link to login page in navbar component

--- client/src/features/nav/Menu/SignedOutLinks.jsx
+++ client/src/features/nav/Menu/SignedOutLinks.jsx
@@ -7,7 +7,7 @@ import Button from '@material-ui/core/Button';
 const SignedOutLinks = ({ login }) => {
   return (
     <Fragment>
-      <Button color="inherit" size="small" onClick={login}>Login</Button>
+      <Button component={Link} to="/login" color="inherit" size="small" onClick={login}>Login</Button>
       <Button component={Link} to="/register" variant="contained" size="small" color="secondary">Sign Up</Button>
     </Fragment>
   )

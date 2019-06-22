fb019e30ccb67ce145c1d21ce4dbc208c456d578
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-06-22 23:48:12 +0200

    Convert App component to class based component
    - Add component lifecycle method to loadUser and getArticles every time the components mounts

--- client/src/app/layout/App.jsx
+++ client/src/app/layout/App.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment, useEffect } from 'react';
-import { Redirect } from 'react-router-dom'
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
 /* MUI Components */
@@ -7,13 +7,16 @@ import { CssBaseline } from '@material-ui/core';
 
 /* Components */
 import NavBar from '../../features/nav/NavBar/NavBar';
-import PostDashboard from '../../features/post/PostDashboard/PostDashboard';
+import ArticleDashboard from '../../features/article/ArticleDashboard/ArticleDashboard';
 import RegisterForm from '../../features/auth/Register/RegisterForm';
 import LoginForm from '../../features/auth/Login/LoginForm';
+import ArticleForm from '../../features/article/ArticleForm/ArticleForm';
+import ArticleDetailsPage from '../../features/article/ArticleDetails/ArticleDetailsPage';
 import PrivateRoute from '../common/util/PrivateRoute';
 
 /* Actions */
 import { loadUser } from '../../features/auth/authActions';
+import { getArticles } from '../../features/article/articleActions';
 
 /* Custom Set Auth Token function */
 import setAuthToken from '../common/util/setAuthToken';
@@ -22,21 +25,31 @@ if (localStorage.token) {
   setAuthToken(localStorage.token)
 }
 
-const App = ({ store }) => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
-  return (
-    <Fragment>
-      <CssBaseline />
-      <NavBar />
-      <Switch>
-        <Route exact path="/register" component={RegisterForm} />
-        <Route exact path="/login" component={LoginForm} />
-        <PrivateRoute exact path="/posts" component={PostDashboard} />
-      </Switch>
-    </Fragment>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.loadUser();
+    this.props.getArticles();
+  }
+  render() {
+    return (
+      <Fragment>
+        <CssBaseline />
+        <NavBar />
+        <Switch>
+          <Route exact path="/register" component={RegisterForm} />
+          <Route exact path="/login" component={LoginForm} />
+          <PrivateRoute exact path="/articles" component={ArticleDashboard} />
+          <PrivateRoute exact path="/article/add" component={ArticleForm} />
+          <PrivateRoute exact path="/article/:id" component={ArticleDetailsPage} />
+        </Switch>
+      </Fragment>
+    )
+  }
 }
 
-export default App;
+const actions = {
+  loadUser,
+  getArticles
+}
+
+export default connect(null, actions)(App);

882bf0ad1536529420a7d809d3a2b6ce018ae122
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-06-22 17:50:16 +0200

    Update rootReducer
    - Add async reducer
    - Update post reducer instead of posts

--- client/src/app/reducers/rootReducer.js
+++ client/src/app/reducers/rootReducer.js
@@ -5,12 +5,14 @@ import { reducer as toastrReducer } from 'react-redux-toastr';
 /* Reducers */
 import authReducer from '../../features/auth/authReducer';
 import postReducer from '../../features/post/postReducer';
+import asyncReducer from '../../features/async/asyncReducer';
 
 const rootReducer = combineReducers({
   form: formReducer,
   auth: authReducer,
   toastr: toastrReducer,
-  post: postReducer
+  post: postReducer,
+  async: asyncReducer
 });
 
 export default rootReducer;
